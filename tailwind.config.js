b22b3fa3afade87251b5f4665cf49a1b23c8a438
mdtalha109 <mdtalha1041@gmail.com>
2024-06-28 21:25:17 +0530

    added theme color

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,23 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          light: '#1976d2',
+          DEFAULT: '#302D43',
+          dark: '#0d8ecf',
+        },
+        primaryMuted: {
+          DEFAULT: '#373A40'
+        },
+
+        error: {
+          DEFAULT: "#f44336"
+        }
+        
+      }
+    },
   },
   plugins: [],
 }
