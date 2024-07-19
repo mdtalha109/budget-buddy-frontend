51ad6043e6e8b89f08c4b7c24d1f9b8f4a9b6a2f
mdtalha109 <mdtalha1041@gmail.com>
2024-07-19 22:32:58 +0530

    fix: exported type from redux store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,19 +1,21 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { Action, ThunkAction, configureStore } from '@reduxjs/toolkit';
 import authReducer, { AuthState } from './authSlice';
 import userSlice, {UserState} from './userSlice';
+import expenseSlice, {ExpenseState} from './expenseSlice';
+import incomeSlice from './incomeSlice';
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
-    user: userSlice
+    user: userSlice,
+    expense: expenseSlice,
+    income: incomeSlice
   },
 });
 
-export type RootState = {
-  auth: AuthState;
-  user: UserState
-};
+export type RootState = ReturnType<typeof store.getState>;
 
 export type AppDispatch = typeof store.dispatch;
+export type AppThunk<ReturnType = void> = ThunkAction<ReturnType, RootState, unknown, Action<string>>;
 
 export default store;
