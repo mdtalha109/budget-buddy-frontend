3e7e60f87dfdfc97cd08d89f7021ba5179db65c4
mdtalha109 <mdtalha1041@gmail.com>
2024-07-20 18:26:40 +0530

    feat: added private route

---
+++ src/components/routes/PrivateRoute.tsx
@@ -0,0 +1,13 @@
+// src/components/PrivateRoute.tsx
+import React from 'react';
+import { Navigate, Outlet } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { RootState } from '../../redux/store';
+
+const PrivateRoute: React.FC = () => {
+   
+  const isAuthenticated = useSelector((state: RootState) => state.auth.isAuthenticated);
+  return isAuthenticated ? <Outlet /> : <Navigate to="/auth/login" />;
+};
+
+export default PrivateRoute;
