2d8e074208f680d1fcfe5cc2b25ffb24111f59ff
mdtalha109 <mdtalha1041@gmail.com>
2024-07-20 18:33:04 +0530

    feat(layout): added sidebar in main layout

---
+++ src/components/shared/Sidebar/index.tsx
@@ -0,0 +1,98 @@
+
+import React, { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+
+import { FaTachometerAlt, FaWallet, FaPiggyBank, FaUserCircle, FaSignOutAlt, FaBars, FaTimes } from 'react-icons/fa';
+import { useTranslation } from 'react-i18next';
+
+const Sidebar: React.FC = () => {
+  const { t } = useTranslation();
+  const [isOpen, setIsOpen] = useState(true);
+
+  const toggleSidebar = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div className={`flex p-2 md:p-4 fixed flex-col h-full  bg-primary  text-white shadow-lg   ${isOpen ? 'w-64' : 'w-[70px]'}`}>
+      <div className=" flex  justify-center items-center">
+        <h1 className={`text-2xl font-bold ${isOpen ? 'block' : 'hidden'}`}>{t("app.title")}</h1>
+        <button className='md:hidden' onClick={toggleSidebar}>
+          {isOpen ? <FaTimes  size={24} /> : <FaBars className={`${isOpen ? '' : 'text-center'}`} size={24} />}
+        </button>
+      </div>
+      <nav className="mt-10 flex-1  ">
+        <ul>
+          <li className="mb-2">
+            <NavLink
+              to="/dashboard"
+              className={({ isActive }) =>
+                `flex items-center  py-2 rounded-lg transition duration-300 ${isActive ? 'bg-gray-700 text-white' : 'hover:bg-gray-700'}
+                ${isOpen ? 'px-2': 'justify-center'}
+                `
+              }
+            >
+              <FaTachometerAlt className={`${isOpen ? 'mr-2': ''}`} />
+              <span className={`${isOpen ? 'inline' : 'hidden'}`}>{t("app.dashboard")}</span>
+            </NavLink>
+          </li>
+          <li className="mb-2">
+            <NavLink
+              to="/expenses"
+              className={({ isActive }) =>
+                `flex items-center  py-2 rounded-lg transition duration-300 ${isActive ? 'bg-gray-700' : 'hover:bg-gray-700'}
+                ${isOpen ? 'px-2': 'justify-center'}
+                `
+              }
+           
+            >
+              <FaWallet className={`${isOpen ? 'mr-2': ''}`} />
+              <span className={`${isOpen ? 'inline' : 'hidden'}`}>{t("expense.title")}</span>
+            </NavLink>
+          </li>
+          <li className="mb-2">
+            <NavLink
+              to="/incomes"
+              className={({ isActive }) =>
+                `flex items-center py-2 rounded-lg transition duration-300 ${isActive ? 'bg-gray-700' : 'hover:bg-gray-700'}
+                ${isOpen ? 'px-2': 'justify-center'}
+                `
+              }
+             
+            >
+              <FaPiggyBank className={`${isOpen ? 'mr-2': ''}`}  />
+              <span className={`${isOpen ? 'inline' : 'hidden'}`}>{t("income.title")}</span>
+            </NavLink>
+          </li>
+          <li className="mb-2">
+            <NavLink
+              to="/profile"
+              className={({ isActive }) =>
+                `flex items-center py-2 rounded-lg transition duration-300 ${isActive ? 'bg-gray-700' : 'hover:bg-gray-700'}
+                ${isOpen ? 'px-2': 'justify-center'}
+                `
+              }
+             
+            >
+              <FaUserCircle className={`${isOpen ? 'mr-2': ''}`} />
+              <span className={`${isOpen ? 'inline' : 'hidden'}`}>{t("app.profile")}</span>
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+      <div className="">
+        <button
+          onClick={() => {
+            // Handle logout logic here
+          }}
+          className="flex items-center w-full px-4 py-2 rounded-lg bg-red-600 hover:bg-red-700 transition duration-300"
+        >
+          <FaSignOutAlt className="mr-3" />
+          <span className={`${isOpen ? 'inline' : 'hidden'}`}>{t("app.logout")}</span>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
---
+++ src/layout/MainLayout.tsx
@@ -0,0 +1,21 @@
+
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+
+import Sidebar from '../components/shared/Sidebar';
+
+const Layout: React.FC = () => {
+  return (
+    <div className="flex min-h-screen h-full ">
+      <Sidebar />
+      <div className="flex-1 ml-[70px] lg:ml-64 dark:bg-primary-dark dark:text-white overflow-x-scroll"> 
+         <div className='p-4  '>
+            <Outlet />
+         </div>
+        
+      </div>
+    </div>
+  );
+};
+
+export default Layout;
