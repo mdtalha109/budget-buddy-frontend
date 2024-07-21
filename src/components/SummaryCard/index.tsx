7246778e8b45d8df9f76fb609ac592625c086f95
mdtalha109 <mdtalha1041@gmail.com>
2024-07-21 23:50:06 +0530

    feat: changed card style and refactored summary card

--- src/components/SummaryCard/index.tsx
+++ src/components/SummaryCard/index.tsx
@@ -1,22 +1,30 @@
-import React from "react";
+import { Card, CardContent, CardHeader, CardSubTitle, CardTitle } from "../ui/Card";
 
 
 type Summary = { title: String; subTitle: String, data: any; icon?: any; tooltip?: string; info?: any };
 
 export default function SummaryCard({ title, subTitle, data, info: Info }: Summary) {
 	return (
-		<div className="rounded-lg border bg-white dark:bg-primary text-card-foreground shadow-sm flex flex-col gap-2">
-			<div className="flex flex-col space-y-1.5 p-4 sm:p-6">
-				<h3 className="whitespace-nowrap text-lg sm:text-xl lg:text-2xl font-semibold leading-none tracking-tight">
-					{title}
-				</h3>
-				<p className="text-xs sm:text-sm dark:text-primaryMuted-dark">
-					{subTitle}
-				</p>
+		<Card className="">
+			<div className=" flex flex-col gap-4">
+				<div>
+					<CardTitle>
+						{title}
+					</CardTitle>
+
+					<CardSubTitle>
+						{subTitle}
+					</CardSubTitle>
+				</div>
+
+				<div>
+					<CardContent>
+						<div className="text-2xl sm:text-3xl lg:text-4xl font-bold">Rs {data}</div>
+					</CardContent>
+				</div>
+
+
 			</div>
-			<div className="p-4 sm:p-6 flex-1 flex items-center ">
-				<div className="text-2xl sm:text-3xl lg:text-4xl font-bold">Rs {data}</div>
-			</div>
-		</div>
+		</Card>
 	);
 }
--- src/components/ui/Card/index.tsx
+++ src/components/ui/Card/index.tsx
@@ -3,14 +3,14 @@ import { cn } from '../../../lib/util';
 
 
 
-const Card = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(({ className, ...props }, ref) => (
-	<div ref={ref} className={cn('rounded-xl border border-border bg-card text-card-foreground', className)} {...props} />
+const Card = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(({ className, children}, ref) => (
+	<div ref={ref} className={cn('bg-white shadow-lg p-4 sm:p-6 flex flex-col space-y-1.5 rounded-xl border border-border', className)}>{children}</div>
 ));
 Card.displayName = 'Card';
 
 const CardHeader = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(
 	({ className, ...props }, ref) => (
-		<div ref={ref} className={cn('flex flex-col space-y-1.5 p-4', className)} {...props} />
+		<div ref={ref} className={cn('flex flex-col space-y-1.5 p-4', className)} {...props} ></div>
 	)
 );
 CardHeader.displayName = 'CardHeader';
@@ -37,13 +37,13 @@ const CardDescription = React.forwardRef<HTMLParagraphElement, React.HTMLAttribu
 CardDescription.displayName = 'CardDescription';
 
 const CardContent = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(
-	({ className, ...props }, ref) => <div ref={ref} className={cn('p-4 pt-0', className)} {...props} />
+	({ className, ...props }, ref) => <div ref={ref} className={cn('', className)} {...props} />
 );
 CardContent.displayName = 'CardContent';
 
 const CardFooter = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(
 	({ className, ...props }, ref) => (
-		<div ref={ref} className={cn(' flex items-center p-4 pt-0', className)} {...props} />
+		<div ref={ref} className={cn(' ', className)} {...props} />
 	)
 );
 CardFooter.displayName = 'CardFooter';
