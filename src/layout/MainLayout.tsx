0a462558f437b65258be46c04a61f314cf7031cb
mdtalha109 <mdtalha1041@gmail.com>
2024-08-28 23:49:30 +0530

    style: changed main background color

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -8,7 +8,7 @@ const Layout: React.FC = () => {
   return (
     <div className="flex min-h-screen h-full ">
       <Sidebar />
-      <div className="flex-1 ml-[70px] lg:ml-64 dark:bg-primary-dark dark:text-white overflow-x-scroll"> 
+      <div className="flex-1 ml-[70px] lg:ml-64 bg-[#f3f5fa] dark:bg-primary-dark dark:text-white overflow-x-scroll"> 
          <div className='p-4  '>
             <Outlet />
          </div>
